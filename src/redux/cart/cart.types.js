9f8cfef9aaf0dccfd91027cfbbae46c7d6250b87
balint <sracz.balint@gmail.com>
2021-11-09 21:50:31 +0100

    created cart-item component and connected our cart dropdown with CartItem reducer

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,6 @@
 const CartActionTypes = {
-    TOOGLE_CART_HIDDEN: ' TOOGLE_CART_HIDDEN',
+    TOOGLE_CART_HIDDEN: 'TOOGLE_CART_HIDDEN',
     ADD_ITEM: 'ADD_ITEM'
-};
-
-export default CartActionTypes;
+  };
+  
+  export default CartActionTypes;
