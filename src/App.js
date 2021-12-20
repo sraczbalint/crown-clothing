64f1845bf136e204475f7993da8a4f827307f2d3
balint <sracz.balint@gmail.com>
2021-12-20 20:24:29 +0100

    updatiing stripe integration and updating our favicon and title

--- src/App.js
+++ src/App.js
@@ -50,7 +50,8 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
-            <Route path='/checkout' component={CheckoutPage} />            <Route exact path='/signin' 
+            <Route path='/checkout' component={CheckoutPage} />            
+            <Route exact path='/signin' 
               render={() => this.props.currentUser 
               ? 
               (<Redirect to='/' />) 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,4 @@
-//import react from "react";
+import React from "react";
 import Directory from "../../components/directory/directory.component";
 import './homepage.style.scss';
 
